9a24e574193cf9ce5374d54c2c077a40937fed6b
Vishwajeet Kale <KaleVis@india.rapidigm.com>
2019-03-01 12:41:57 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	@GetMapping(value = "/")
+	public String testController() {
+		return "Vishwa";
+	}
+}
